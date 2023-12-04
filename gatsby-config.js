9978d48a0802caf0a5b734843d0197a5ae6793cc
stevekim72 <steve.kim72@yahoo.com>
2023-12-04 14:47:04 -0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,53 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Steve Kim`,
+    description: `Combining product design expertise with web development experience, I offer a unique perspective to create engaging, user-friendly websites, utilizing both technical and creative problem-solving skills.`,
+    author: `Steve Kim`,
+    siteUrl: `https://stevekimd.io`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon-v2-1.png",
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svg/,
+          omitKeys: [
+            "xmlnsDc",
+            "xmlnsCc",
+            "xmlnsRdf",
+            "xmlnsSvg",
+            "xmlnsSodipodi",
+            "xmlnsInkscape",
+            "xmlnsSerif",
+            "serifId",
+          ],
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-anchor-links",
+      options: {
+        offset: -100,
+      },
+    },
+  ],
 };
